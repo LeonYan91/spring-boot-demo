73d49464ef7c38be5ca2adcc4a01bd4029316f4b
leon <leonyan91@163.com>
2019-08-06 17:24:27 +0800

    dev: add jenkinsfile

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -9,4 +9,7 @@ echo '11111111'
 ls /opt/tomcat/latest/webapps/
 echo '22222222'
 ls ./target/
+echo '33333333'
+whoami
+echo '44444444'
 mv ./target/spring-boot-demo-0.0.1-SNAPSHOT.war /opt/tomcat/latest/webapps/bootdemo.war
