7727c976fd67f4f6c0b0ea3a868268369b6f9cee
leon <leonyan91@163.com>
2019-08-06 17:03:13 +0800

    dev: add jenkinsfile

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -5,5 +5,8 @@ echo 'start deploy'
 #mvn package
 
 pwd
-
+echo '11111111'
+ls /opt/tomcat/latest/webapps/
+echo '22222222'
+ls ./target/
 mv ./target/spring-boot-demo-0.0.1-SNAPSHOT.war /opt/tomcat/latest/webapps/bootdemo.war
