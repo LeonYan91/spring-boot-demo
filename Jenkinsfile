48960e98a7a6d7c57ea7dca1b912f1be0a7c2765
leon <leonyan91@163.com>
2019-08-30 09:55:25 +0800

    dev: add jenkinsfile

--- Jenkinsfile
+++ Jenkinsfile
@@ -4,7 +4,7 @@ pipeline {
         stage('build') {
             steps {
                 sh 'mvn package'
-
+            }
         }
 
         stage('deploy') {
--- src/main/java/com/leon/springbootdemo/hello/HelloController.java
+++ src/main/java/com/leon/springbootdemo/hello/HelloController.java
@@ -10,6 +10,6 @@ public class HelloController  {
 
     @RequestMapping("/hello")
     public String hello(){
-        return "hello world success 8";
+        return "hello world success 9";
     }
 }
